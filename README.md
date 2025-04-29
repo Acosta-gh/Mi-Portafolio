ce0abaab11b7b1eb5fb0c09086774a0083e09d25
Cristian Acosta <79601880+Acosta-gh@users.noreply.github.com>
2025-04-29 20:49:39 -0300

    Se agregó un language menu al header

--- README.md
+++ README.md
@@ -1,25 +1,26 @@
-# Portafolio Personal
+# 💼 Portafolio Personal
 
 Este es mi portafolio personal, diseñado y desarrollado con un enfoque en la simplicidad, el rendimiento y la accesibilidad. He utilizado herramientas esenciales para garantizar que sea liviano, eficiente y fácil de desplegar, incluso en plataformas como GitHub Pages.
 
-## Tecnologías Utilizadas
+## 💻 Tecnologías Utilizadas
 
 - **HTML**: Para estructurar el contenido de manera semántica.
 - **SCSS**: Para crear estilos modulares, reutilizables y fáciles de mantener.
 - **JavaScript**: Para implementar interactividad y animaciones fluidas.
 
-## Características
+## 🔨 Características
 
 - Diseño minimalista, responsivo y visualmente atractivo.
 - Código optimizado para un rendimiento rápido y una fácil mantenibilidad.
 - Compatible con cualquier servidor estático para un despliegue sin complicaciones.
 - Experiencia de usuario intuitiva, accesible y agradable.
 
-## Capturas de Pantalla
+## 📸 Capturas de Pantalla
 
 *(Próximamente)*
 
-## Créditos
+## 🗒 Créditos
 
-- **Logo**[https://www.svgrepo.com/svg/487218/code]: Autor: Neuicons | Licencia: MIT License
-- **Iconos**: Proporcionados por [SimpleIcons](https://simpleicons.org), así como por [Bootstrap Icons](https://icons.getbootstrap.com).
+- **Code SVG Vector**: [Fuente](https://www.svgrepo.com/svg/487218/code) | Autor: Neuicons | Licencia: MIT License
+- **Arrow Down SVG Vector**: [Fuente](https://www.svgrepo.com/svg/521469/arrow-down) | Autor: Konstantin Filatov | Licencia: CC Attribution License
+- **Iconos**: Proporcionados por [SimpleIcons](https://simpleicons.org) y [Bootstrap Icons](https://icons.getbootstrap.com)
--- scripts/modules/animations.js
+++ scripts/modules/animations.js
@@ -1,24 +1,39 @@
 export function burguerAnimation() {
-    const burguer = document.querySelector('.header__nav-burguer');
-    const nav = document.querySelector('.header__nav-list');
     const header = document.querySelector('.header');
+    const burguer = document.querySelector('.header__nav-burguer');
     const navItems = document.querySelectorAll('.header__nav-item');
+    const nav = document.querySelector('.header__nav-list');
+    const langMenu = document.querySelector(".header__nav-langmenu")
+    const langItems = document.querySelectorAll('.header__nav-langitem');
+    const langlist = document.querySelector(".header__nav-langlist")
+    
     if (!burguer || !nav || !header || navItems.length === 0) {
         console.error("No se encontraron los elementos necesarios");
         return;
+    }
+
+    function removeOtherItemsClasses(navItems){
+        navItems.forEach(item => {
+            item.classList.remove("item--active");
+        });
     }
 
     burguer.addEventListener('click', () => {
         burguer.classList.toggle('header__nav-burguer--active');
         nav.classList.toggle('header__nav--active');
         header.classList.toggle('header--active');
+
+        // Cerrar el menú de idiomas antes de cerrar el menú burguer
+        langlist.classList.remove("header__nav-langlist--active")
+        langMenu.classList.remove("header__nav-langmenu--active")
     });
     
-    function removeOtherItemsClasses(navItems){
-        navItems.forEach(item => {
-            item.classList.remove("item--active");
-        });
-    }
+    langMenu.addEventListener("click", () => {
+        console.log("langmenu clicked")
+        langlist.classList.toggle("header__nav-langlist--active")
+        langMenu.classList.toggle("header__nav-langmenu--active")
+
+    });
 
     // Si hace click en algún item, el menú cierra automaticamente / If a nav item is clicked, the menu closes automatically
     navItems.forEach((item) => {
@@ -31,4 +46,14 @@ export function burguerAnimation() {
             item.classList.toggle("item--active")
         });
     });
+
+    langItems.forEach((item) => {
+        item.addEventListener('click', () => {
+            console.log("lang item clicked")
+            langMenu.classList.toggle("header__nav-langmenu--active")
+            langlist.classList.toggle("header__nav-langlist--active")
+        });
+    });
+
+    
 }
