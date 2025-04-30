b0d8464232167dabf269269c32a73386a77b0423
Cristian Acosta <79601880+Acosta-gh@users.noreply.github.com>
2025-04-29 21:17:12 -0300

    Cambios menores

--- scripts/modules/animations.js
+++ scripts/modules/animations.js
@@ -5,55 +5,56 @@ export function burguerAnimation() {
     const nav = document.querySelector('.header__nav-list');
     const langMenu = document.querySelector(".header__nav-langmenu")
     const langItems = document.querySelectorAll('.header__nav-langitem');
-    const langlist = document.querySelector(".header__nav-langlist")
-    
+    const langList = document.querySelector(".header__nav-langlist")
+
     if (!burguer || !nav || !header || navItems.length === 0) {
         console.error("No se encontraron los elementos necesarios");
         return;
     }
 
-    function removeOtherItemsClasses(navItems){
+    function deactivateAllNavItems() {
         navItems.forEach(item => {
             item.classList.remove("item--active");
         });
     }
 
-    burguer.addEventListener('click', () => {
+    function toggleLangMenu() {
+        langMenu.classList.toggle("header__nav-langmenu--active")
+        langList.classList.toggle("header__nav-langlist--active")
+    }
+
+    function closeLangMenu() {
+        langList.classList.remove("header__nav-langlist--active")
+        langMenu.classList.remove("header__nav-langmenu--active")
+    }
+
+    function toggleMenuElements() {
         burguer.classList.toggle('header__nav-burguer--active');
         nav.classList.toggle('header__nav--active');
         header.classList.toggle('header--active');
+    }
 
-        // Cerrar el menú de idiomas antes de cerrar el menú burguer
-        langlist.classList.remove("header__nav-langlist--active")
-        langMenu.classList.remove("header__nav-langmenu--active")
+    burguer.addEventListener('click', () => {
+        toggleMenuElements()
+        closeLangMenu()
     });
-    
-    langMenu.addEventListener("click", () => {
-        console.log("langmenu clicked")
-        langlist.classList.toggle("header__nav-langlist--active")
-        langMenu.classList.toggle("header__nav-langmenu--active")
 
+    langMenu.addEventListener("click", () => {
+        toggleLangMenu()
     });
 
     // Si hace click en algún item, el menú cierra automaticamente / If a nav item is clicked, the menu closes automatically
     navItems.forEach((item) => {
         item.addEventListener('click', () => {
-            burguer.classList.toggle('header__nav-burguer--active');
-            nav.classList.toggle('header__nav--active');
-            header.classList.toggle('header--active');
-            
-            removeOtherItemsClasses(navItems)
+            toggleMenuElements();
+            deactivateAllNavItems()
             item.classList.toggle("item--active")
         });
     });
 
     langItems.forEach((item) => {
         item.addEventListener('click', () => {
-            console.log("lang item clicked")
-            langMenu.classList.toggle("header__nav-langmenu--active")
-            langlist.classList.toggle("header__nav-langlist--active")
+            toggleLangMenu()
         });
     });
-
-    
 }
