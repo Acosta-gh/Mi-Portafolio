3aa7de6f9c76caa6ebdb871026abeb6edc379cf1
Cristian Acosta <79601880+Acosta-gh@users.noreply.github.com>
2025-06-02 07:41:53 -0300

    Ahora si, LibreJS entiende la licencia de la página

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,10 @@
-
-// @license Apache-2.0
-/*
+/**
+ * Copyright 2025 Cristian Darío Acosta
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
@@ -12,8 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-
 document.addEventListener('DOMContentLoaded', () => {
     /**
      * Función para aplicar un efecto de fade-in (aparecer lentamente)
@@ -201,7 +200,7 @@ document.addEventListener('DOMContentLoaded', () => {
             handleScrollAnimation();
         });
     }
-    
+
     /**
      * Retorna un saludo basado en la hora actual y el idioma del documento.
      * @returns {string} Saludo adecuado para la hora y el idioma.
@@ -219,7 +218,7 @@ document.addEventListener('DOMContentLoaded', () => {
             } else {
                 greeting = "Good evening";
             }
-        } else { 
+        } else {
             if (hour < 12) {
                 greeting = "Buenos días";
             } else if (hour < 18) {
@@ -294,4 +293,3 @@ document.addEventListener('DOMContentLoaded', () => {
     // Inicializa animación al hacer scroll
     jsScroll();
 });
-// @license-end
